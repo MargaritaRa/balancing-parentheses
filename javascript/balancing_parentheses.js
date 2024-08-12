c540fe9ac56182ccc23e9bf73b1ecd097731098f
Margarita Ramirez <margarita26.mr@gmail.com>
2024-08-12 13:05:37 -0400

    missing elements

--- javascript/balancing_parentheses.js
+++ javascript/balancing_parentheses.js
@@ -1,5 +1,28 @@
 function balancingParentheses(string) {
-  // type your code here
+  //  initalize missing to 0 (will store unmatched closing parens count)
+  let missing = 0;
+  //  initalize openings to 0 (will store the parens count)
+  let openings = 0;
+
+  //  iterate over sting
+  for (let i = 0; i < string.length; ++i){
+    //  if char == "("
+    if (string[i] === "(") {
+      // increment openings
+      ++openings;
+      continue;
+    }
+    
+    if (openings > 0) {
+      --openings;
+    } else {
+      // if openings is 0
+      // increment missing
+      ++missing;
+    }
+  }
+
+  return missing + openings;
 }
 
 if (require.main === module) {
@@ -16,9 +39,46 @@ if (require.main === module) {
 
   console.log("Expecting: 1");
   console.log(balancingParentheses(')'));
+
+  console.log("");
+
+  console.log("Expecting: 0");
+  console.log(balancingParentheses('()'));
+
+  console.log("");
+
+  console.log("Expecting: 1");
+  console.log(balancingParentheses('('));
+
+  console.log("");
+
+  console.log("Expecting: 2");
+  console.log(balancingParentheses(')('));
+
+  console.log("");
+
+  console.log("Expecting: 1");
+  console.log(balancingParentheses(')()'));
+
+  console.log("");
+
+  console.log("Expecting: 2");
+  console.log(balancingParentheses(')((((()))((())))()(()))('));
+
+  console.log("");
+
+  console.log("Expecting: 3");
+  console.log(balancingParentheses(')))'));
+
+  console.log("");
+
+  console.log("Expecting: 3");
+  console.log(balancingParentheses('((('));
 }
 
 module.exports = balancingParentheses;
 
 // Please add your pseudocode to this file
+// the length of the string needs to be at least one
+// check each line and determine
 // And a written explanation of your solution
